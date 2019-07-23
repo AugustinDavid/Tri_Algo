6967d22ae67d215f253880a36d695259f7aecbc4
AugustinDavid <davdav07210@gmail.com>
2019-07-23 16:23:32 +0200

    implémentation func de tri merge et quicksort

--- sorting_functions.py
+++ sorting_functions.py
@@ -89,16 +89,76 @@ def selectionsort(A, *args, **kwargs):
 
 
 
-def mergesort(A, start, end):
+#def mergesort(A, start, end):
+def mergesort(A, *args, **kwargs):
     """Merge sort."""
-    print(" # TODO # ")
-    yield A
+
+   # On teste si le tableau est égal a 1
+    if len(A) == 1:
+        return A
+
+    #on coupe le tableau A en deux en se servant de sa longueur
+    tab_1 = A[:int((len(A)/2))]
+    tab_2 = A[int((len(A)/2)):]
+
+    #on applique la fonction mergesort de façon récursive ce qui permet de découper les tableau pour pouvoir ensuite appliquer le tri
+    tab_1 = mergesort(tab_1)
+    tab_2 = mergesort(tab_2)
 
 
-def quicksort(A, start, end):
+    return merge(tab_1, tab_2)
+
+
+
+#Fonction qui tri les tableaux, le mieux est de commencer par celle ci dans le tri par fusion pour voir le résultat dans la fonction mergesort
+def merge(tab_A, tab_B):
+    C = []
+
+    while tab_A != [] and tab_B != []:#tant que A et B ne sont pas vide
+        #Si A est sup a B on ajoute b a la fin de la liste C sinon ce sera A
+        if tab_A[0] > tab_B[0]:
+
+            C.append(tab_B[0])
+            tab_B.pop(0)
+        else:
+            C.append(tab_A[0])
+            tab_A.pop(0)
+    #il faut créer deux while dans le cas où l'un des 2 tableaux serait vide ce qui bloquerai le 1er while
+    while tab_A != []:
+        C.append(tab_A[0])
+        tab_A.pop(0)
+    while tab_B != []:
+        C.append(tab_B[0])
+        tab_B.pop(0)
+
+    return C
+
+
+
+#def quicksort(A, start, end):
+def quicksort(A, *args, **kwargs):
     """In-place quicksort."""
-    print(" # TODO # ")
-    yield A
+    tour = 1
+
+    if A == []:
+        return []
+
+    else:
+        pivot = A[0]
+        print(pivot)
+        A1 = []
+        A2 = []
+        for element in A[1:]:
+            if element < pivot:
+                A1.append(element)
+                print(A1)
+            else:
+                A2.append(element)
+                print(A2)
+        tour += 1
+        return quicksort(A1) + [pivot] + quicksort(A2)
+
+
 
 
 def shellsort(A, *args, **kwargs):#tri shellsort : www.geeksforgeeks
@@ -131,8 +191,8 @@ if __name__ == "__main__":
     test_data = [8, 5, 9, 34, 22, 12, 4, 2]
 
 
-    #for a in shellsort(test_data):
-        #print(a)
+    for a in quicksort(test_data):
+        print(a)
 
     # for a in shellsort(test_data):
     #     print(a)
